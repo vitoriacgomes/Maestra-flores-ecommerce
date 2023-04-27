cfbc9795cf6f4f9afa58e621d4e833b01e059a41
vitoria <vi21cgomes@gmail.com>
2023-04-27 18:34:00 -0300

    about

--- src/componentes/Sobre.js
+++ src/componentes/Sobre.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import flores from "./flores-sobre.jpeg"
+import flores from "./Flor.png"
 
 const Sobre = () => {
   return (
     <div className='sobre'>
       <div className='container'>
-        <div className='col-6 p-25'>
-          <h3>Sobre Nós</h3>
-          <h1>BEM-VINDO AO MAESTRA FLORES</h1>
-          <p>Aqui nos buscamos ter o maior sistema de gestão de qualidade em noassas flores. Elas são escolhidas a mão, para você presentear quem mais ama.</p>
-          <div className='sobre-btn'>
-            <a className='btn btn-smart'>Leia mais</a>
-          </div>
+        <div className='row'>
+          <div className='col-6 p-25'>
+            <h3>Sobre Nós</h3>
+            <h1>BEM-VINDO AO MAESTRA FLORES</h1>
+            <p>Aqui nos buscamos ter o maior sistema de gestão de qualidade em noassas flores. Elas são escolhidas a mão, para você presentear quem mais ama.</p>
+            <div className='sobre-btn'>
+              <a className='btn btn-smart'>Leia mais</a>
+            </div>
           </div>
           <div className='col-6'>
             <div className='sobre-img'><img src={flores}/></div>
           </div>
+        </div>
       </div>
     </div>
   )
