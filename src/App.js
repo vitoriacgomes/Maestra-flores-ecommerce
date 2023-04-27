d47b9bc36f8bf74516a0bf3763fcb4cbdb8743f2
vitoria <vi21cgomes@gmail.com>
2023-04-26 22:31:43 -0300

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from "./componentes/Header"
+import Sobre from "./componentes/Sobre"
 
 function App() {
   return (
     <div>
       <Header/>
+      <Sobre/>
     </div>
   );
 }
---
+++ src/componentes/Sobre.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Sobre = () => {
+  return (
+    <div className='sobre'>
+      <div className='container'>
+        <h3>Sobre Nós</h3>
+        <h1>BEM-VINDO AO MAESTRA FLORES</h1>
+        <p>Aqui nos buscamos ter o maior sistema de gestão de qualidade em noassas flores. Elas são escolhidas a mão, para você presentear quem mais ama.</p>
+        <div className='sobre-btn'>
+          <a className='btn btn-smart'>Leia mais</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sobre
