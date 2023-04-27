b71a5e614702d51646b24b85adf0be5e0ee3440c
vitoria <vi21cgomes@gmail.com>
2023-04-26 21:47:46 -0300

    home

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 const Header = () => {
   return (
     <div className='banner'>
+        <NavBar />
         <div className='banner_content'>
             <div className='container'>
                 <div className='banner-texto'>
                 <h3>Flores Devlivery</h3>
                 <h1>MAESTRA FLORES</h1>
                 <p>A melhores flores do mercado você encontra aqui. Atuamos em toda area metropolitana</p>
+                <div className='banner-btn'>
+                    <a href='' className='btn btn-smart'>Entrega já</a>
+                </div>
                 </div>
             </div>
         </div>
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <div className='navbar-texto'>
+            <ul className='navbar-ul'>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>Produtos</a></li>
+                <li><a href=''>Carrinho</a></li>
+                <li><a href=''>Nosso Contato</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
