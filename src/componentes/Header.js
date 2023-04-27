daa680de34156230469b358cb6a385a79e9bf7b7
vitoria <vi21cgomes@gmail.com>
2023-04-26 21:01:45 -0300

    init commit

---
+++ src/componentes/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='banner'>
+        <div className='banner_content'>
+            <div className='container'>
+                <div className='banner-texto'>
+                <h3>Flores Devlivery</h3>
+                <h1>MAESTRA FLORES</h1>
+                <p>A melhores flores do mercado você encontra aqui. Atuamos em toda area metropolitana</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
