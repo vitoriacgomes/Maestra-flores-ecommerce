0e3c2dc1de565d33baffb9cf043879988da3abd6
vitoria <vi21cgomes@gmail.com>
2023-05-03 18:40:04 -0300

    title contato

--- src/componentes/Contato.js
+++ src/componentes/Contato.js
@@ -4,7 +4,7 @@ import caminhao from "./img/caminhao-de-flores.jpg"
 const Contato = () => {
   return (
     <div className='contato'>
-        <h1><span>nosso contato</span></h1>
+        <div className='contato-text'><h1 className=''>Nosso Contato</h1></div>
         <div className='contato-row'>
             <form>
                 <input type='text' placeholder=' nome' className='box'></input>
